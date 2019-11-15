4442cc7fa833ec068889213883c2ab5266008754
Rich Chang <rich_chang@outlook.com>
2019-11-15 16:38:51 +0800

    Add behavior on seek bar and text change

--- app/src/main/java/com/richc/eggtimer/MainActivity.java
+++ app/src/main/java/com/richc/eggtimer/MainActivity.java
@@ -3,12 +3,55 @@ package com.richc.eggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+        timerTextView.setText("00:30");
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                int min=1;
+                int timerValue=1;
+
+                if (i < min) {
+                    timerSeekBar.setProgress(timerValue);
+                    timerValue = min;
+
+                } else {
+                    timerValue = i;
+                }
+
+                String mm = String.format("%02d", timerValue/60);
+                String ss = String.format("%02d", timerValue%60);
+
+                timerTextView.setText(mm+":"+ss);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 }
